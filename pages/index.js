e18dd0d912e325fe4e9dad5f3bee4a8523389040
uzair <uzairchaudhary.30@gmail.com>
2023-07-31 12:52:18 +0500

    authentication

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
+export default function Home() {
+  const { data: session } = useSession()
+  if (!session){
+  return (
+    <div className='bg-blue-900 w-screen h-screen flex items-center' > 
+      <div className='text-center w-full'>
+          <button className='bg-white p-2 px-4 rounded-lg'onClick={() => signIn()}>Login with Google</button>
+      </div>
+    </div>
+  )
+};
+return (
+  <div className='bg-blue-900 w-screen h-screen flex items-center' >
+    <div className='text-center w-full'>
+         Signed in as {session.user.email} <br/>
+        <button className='bg-white p-2 px-4 rounded-lg' onClick={() => signOut()}>Logout</button>
+    </div>
+  </div>
+)
+
+}
