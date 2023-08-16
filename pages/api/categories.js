af833c41986771b37ec75e7ac36aa03dc5e5b063
uzair <uzairchaudhary.30@gmail.com>
2023-08-16 18:12:28 +0500

    EditAndDeleteCategories

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -8,10 +8,36 @@ export default async function handle( req, res){
     if (method==='GET'){
         res.json(await Category.find({}).populate('parent'))
     }
+
+
     if (method==='POST'){
         const {name, parentCategory} = req.body;
         res.json(await Category.create({
             name, 
             parent:parentCategory}))
     }
+
+
+    if (method==='PUT'){
+        const {name, parentCategory, _id} = req.body;
+
+        res.json(await Category.updateOne({_id}, {
+            name,
+            parent:parentCategory,
+        }))
+    }
+    // delete category by id
+    if (method==='DELETE'){
+        try{
+            const {_id} = req.query
+            await Category.deleteOne({_id})
+            res.json({message:'Category deleted'})
+        }
+        catch(error){
+            res.json({message:error})
+        }
+    }
+
+
+
 }
--- pages/categories.js
+++ pages/categories.js
@@ -1,12 +1,15 @@
 import Layout from "@/components/layout";
 import axios from "axios";
 import {useState, useEffect} from 'react';
-
+import SweetAlert from 'react-bootstrap-sweetalert';
+import Swal from 'sweetalert2'
 export default function Categories(){
     
     const [name, setname] = useState('');
     const [categories, setcategories] = useState([]);
     const [parentCategory, setparentCategory] = useState('');
+
+    const [editedCategory, seteditedCategory] = useState(null);
 
     useEffect(() => {
         
@@ -23,15 +26,66 @@ export default function Categories(){
 
     async function saveCategory(e){
         e.preventDefault()
-        await axios.post('/api/categories', {name, parentCategory})
+        const data = {name, parentCategory}
+        if (editedCategory){
+            data._id = editedCategory._id
+            await axios.put('/api/categories', data)
+            seteditedCategory(null)
+
+        }
+        else {
+        await axios.post('/api/categories', data) 
+        }
+
+        setparentCategory('')
         setname('')
-        fetchCategories()
+        fetchCategories();
     }
+
+    function editCategory(category){
+        seteditedCategory(category)
+        setname(category.name)
+        setparentCategory(category.parent?._id)
+
+    
+    }
+
+    
+    
+    function deleteCategory(category){
+        
+        Swal.fire({
+            title: 'Are you sure?',
+            text: `You want to delete ${category.name} category?`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d44',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Yes, delete it!'
+          }).then(async ( result) => {
+            if (result.isConfirmed) {
+                const {_id} = category
+                await axios.delete('/api/categories?_id='+_id, {_id})
+                fetchCategories()
+                Swal.fire(
+                'Deleted!',
+                `${category.name} Category has been deleted successfully`,
+                'success'
+              )
+            }
+          })
+        
+          
+      
+    }
+    
+
 
     return(
         <Layout>
             <h1>Categories</h1>
-            <label>New Category Name</label>
+
+            <label> {editedCategory ? `Edit Category ${editedCategory.name}` : "Create New Category"}</label>
             <form className="flex gap-1" onSubmit={saveCategory}>
                 <input 
                 className="mb-0" 
@@ -68,8 +122,8 @@ export default function Categories(){
                         <tr key={category._id}>
                             <td>{category.name}</td>
                             <td>{category?.parent?.name}</td>
-                            <td><button className="btn-primary py-1 bg-blue-900 text-white mr-1">Edit</button>
-                            <button className="btn-primary py-1 bg-red-900 text-white">Delete</button>
+                            <td><button className="btn-primary py-1 bg-blue-900 text-white mr-1" onClick={()=> editCategory(category)}>Edit</button>
+                            <button className="btn-primary py-1 bg-red-800 text-white" onClick={()=> deleteCategory(category)}>Delete</button>
 </td>
                         </tr>
                     ))}
@@ -78,3 +132,4 @@ export default function Categories(){
         </Layout>
     )
 }
+
